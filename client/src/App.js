d2bd167167fc492334216415700160a70d456c06
Alex Wilmer <djazium@gmail.com>
2018-04-18 09:45:40 -0400

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+class App extends React.Component {
   componentDidMount() {
-    this.socket = new WebSocket(process.env.REACT_APP_WS || `ws://localhost:8080`);
+    // Create WebSocket connection.
+    this.socket = new WebSocket(`ws://${process.env.SERVER_HOST}`);
+
+    // Connection opened
+    this.socket.addEventListener('open', function (event) {
+        this.socket.send('Hello Server!');
+    });
 
     // Listen for messages
-    this.socket.addEventListener('message', async event => {
-      const audioCtx = new AudioContext()
-      const response = await fetch('/hi_everybody.wav')
-      const arrayBuffer = await response.arrayBuffer()
-      const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer)
-
-      const source = audioCtx.createBufferSource()
-      source.buffer = audioBuffer
-      source.connect(audioCtx.destination)
-      source.start()
+    this.socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
     });
   }
-
   render() {
-    return (
-      <div className="App">
-        <button onClick={() => { this.socket.send('hello!') }}>
-          poke!
-        </button>
-      </div>
-    );
+    return <h1>
+      <button onClick={() => {
+        this.socket.send('hey i clicked a button')
+      }}>
+      click me 
+    </button>
+
+      Hello App class</h1>
   }
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// class App extends Component {
+//   componentDidMount() {
+//     this.socket = new WebSocket(process.env.REACT_APP_WS ||   `ws://localhost:8080`);
+//
+//     // Listen for messages
+//     this.socket.addEventListener('message', async event => {
+//       const audioCtx = new AudioContext()
+//       const response = await fetch('/hi_everybody.wav')
+//       const arrayBuffer = await response.arrayBuffer()
+//       const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer)
+//
+//       const source = audioCtx.createBufferSource()
+//       source.buffer = audioBuffer
+//       source.connect(audioCtx.destination)
+//       source.start()
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={() => { this.socket.send('hello!') }}>
+//           poke!
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
--- server/basic.js
+++ server/basic.js
@@ -3,13 +3,13 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
+  // ws.on('message', function incoming(message) {
+  //   wss.clients.forEach(function each(client) {
+  //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+  //       client.send(message);
+  //     }
+  //   });
+  // });
 
   console.log('new connection!')
 });
